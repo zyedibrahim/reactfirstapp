f6ef36a1ca480a12e3152515db2de7c240bfc79a
zyedibrahim <zyedibrahim77@gmail.com>
2024-08-26 09:41:35 -0700

    updated movie

--- src/App.jsx
+++ src/App.jsx
@@ -3,34 +3,138 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  let bd = [
+  let moveilist = [
     {
-      id: 1,
-      name: "jack1",
-      img: "https://img.freepik.com/free-photo/portrait-successful-man-having-stubble-posing-with-broad-smile-keeping-arms-folded_171337-1267.jpg",
+      name: "Vikram",
+      poster:
+        "https://m.media-amazon.com/images/M/MV5BMmJhYTYxMGEtNjQ5NS00MWZiLWEwN2ItYjJmMWE2YTU1YWYxXkEyXkFqcGdeQXVyMTEzNzg0Mjkx._V1_.jpg",
+      rating: 8.4,
+      summary:
+        "Members of a black ops team must track and eliminate a gang of masked murderers.",
+    },
+
+    {
+      name: "Iron man 2",
+      poster:
+        "https://m.media-amazon.com/images/M/MV5BMTM0MDgwNjMyMl5BMl5BanBnXkFtZTcwNTg3NzAzMw@@._V1_FMjpg_UX1000_.jpg",
+      rating: 7,
+      summary:
+        "With the world now aware that he is Iron Man, billionaire inventor Tony Stark (Robert Downey Jr.) faces pressure from all sides to share his technology with the military. He is reluctant to divulge the secrets of his armored suit, fearing the information will fall into the wrong hands. With Pepper Potts (Gwyneth Paltrow) and Rhodes (Don Cheadle) by his side, Tony must forge new alliances and confront a powerful new enemy.",
     },
     {
-      id: 2,
-      name: "jack2",
-      img: "https://t4.ftcdn.net/jpg/03/83/25/83/360_F_383258331_D8imaEMl8Q3lf7EKU2Pi78Cn0R7KkW9o.jpg",
+      name: "No Country for Old Men",
+      poster:
+        "https://upload.wikimedia.org/wikipedia/en/8/8b/No_Country_for_Old_Men_poster.jpg",
+      rating: 8.1,
+      summary:
+        "A hunter's life takes a drastic turn when he discovers two million dollars while strolling through the aftermath of a drug deal. He is then pursued by a psychopathic killer who wants the money.",
     },
     {
-      id: 3,
-      name: "jack3",
-      img: "https://www.shutterstock.com/image-photo/portrait-suspicious-girl-stylish-hairdo-260nw-2419831495.jpg",
+      name: "Jai Bhim",
+      poster:
+        "https://m.media-amazon.com/images/M/MV5BY2Y5ZWMwZDgtZDQxYy00Mjk0LThhY2YtMmU1MTRmMjVhMjRiXkEyXkFqcGdeQXVyMTI1NDEyNTM5._V1_FMjpg_UX1000_.jpg",
+      summary:
+        "A tribal woman and a righteous lawyer battle in court to unravel the mystery around the disappearance of her husband, who was picked up the police on a false case",
+      rating: 8.8,
+    },
+    {
+      name: "The Avengers",
+      rating: 8,
+      summary:
+        "Marvel's The Avengers (classified under the name Marvel Avengers\n Assemble in the United Kingdom and Ireland), or simply The Avengers, is\n a 2012 American superhero film based on the Marvel Comics superhero team\n of the same name.",
+      poster:
+        "https://terrigen-cdn-dev.marvel.com/content/prod/1x/avengersendgame_lob_crd_05.jpg",
+    },
+    {
+      name: "Interstellar",
+      poster: "https://m.media-amazon.com/images/I/A1JVqNMI7UL._SL1500_.jpg",
+      rating: 8.6,
+      summary:
+        "When Earth becomes uninhabitable in the future, a farmer and ex-NASA\n pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team\n of researchers, to find a new planet for humans.",
+    },
+    {
+      name: "Baahubali",
+      poster: "https://flxt.tmsimg.com/assets/p11546593_p_v10_af.jpg",
+      rating: 8,
+      summary:
+        "In the kingdom of Mahishmati, Shivudu falls in love with a young warrior woman. While trying to woo her, he learns about the conflict-ridden past of his family and his true legacy.",
+    },
+    {
+      name: "Ratatouille",
+      poster:
+        "https://resizing.flixster.com/gL_JpWcD7sNHNYSwI1ff069Yyug=/ems.ZW1zLXByZC1hc3NldHMvbW92aWVzLzc4ZmJhZjZiLTEzNWMtNDIwOC1hYzU1LTgwZjE3ZjQzNTdiNy5qcGc=",
+      rating: 8,
+      summary:
+        "Remy, a rat, aspires to become a renowned French chef. However, he fails to realise that people despise rodents and will never enjoy a meal cooked by him.",
+    },
+
+    {
+      name: "Thor: Ragnarok",
+      poster:
+        "https://m.media-amazon.com/images/M/MV5BMjMyNDkzMzI1OF5BMl5BanBnXkFtZTgwODcxODg5MjI@._V1_.jpg",
+      summary:
+        "When Earth becomes uninhabitable in the future, a farmer and ex-NASA\\n pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team\\n of researchers, to find a new planet for humans.",
+      rating: 8.8,
+    },
+    {
+      name: "Ratatouille",
+      poster:
+        "https://resizing.flixster.com/gL_JpWcD7sNHNYSwI1ff069Yyug=/ems.ZW1zLXByZC1hc3NldHMvbW92aWVzLzc4ZmJhZjZiLTEzNWMtNDIwOC1hYzU1LTgwZjE3ZjQzNTdiNy5qcGc=",
+      rating: 8,
+      summary:
+        "Remy, a rat, aspires to become a renowned French chef. However, he fails to realise that people despise rodents and will never enjoy a meal cooked by him.",
+    },
+
+    {
+      name: "Thor: Ragnarok",
+      poster:
+        "https://m.media-amazon.com/images/M/MV5BMjMyNDkzMzI1OF5BMl5BanBnXkFtZTgwODcxODg5MjI@._V1_.jpg",
+      summary:
+        "When Earth becomes uninhabitable in the future, a farmer and ex-NASA\\n pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team\\n of researchers, to find a new planet for humans.",
+      rating: 8.8,
     },
   ];
-  // app change jjdafadf
+
   return (
-    <div>
-      {bd.map((ele, id) => (
-        <Msg key={ele.id} name={ele.name} img={ele.img} id={id} />
+    <div className="mlistcon">
+      {moveilist.map((moviedata, id) => (
+        <MovieContainer key={id} moviedata={moviedata} />
       ))}
     </div>
   );
 }
 
-function Msg({ name, img, id }) {
+function MovieContainer({ id, moviedata }) {
+  var [count, setcount] = useState(0);
+  let style = {
+    color: moviedata.rating > 7 ? "green" : "red",
+  };
+  let [show, setshow] = useState(true);
+
+  let showstyle = {
+    display: show ? "block" : "none",
+  };
+
+  return (
+    <div key={id} className="mcon">
+      <img className="poster" src={moviedata.poster} alt={moviedata.name} />
+      <div className="mnr">
+        <div>{moviedata.name}</div>
+        <div style={style}>{moviedata.rating}</div>
+      </div>
+      <button onClick={() => setshow(!show)}>toggle</button>
+      <div style={showstyle} className="summary">
+        {moviedata.summary}
+      </div>
+      <div className="lbutton">
+        <button onClick={() => setcount(count + 1)}>{count}like</button>
+        <button>dislike</button>
+      </div>
+    </div>
+  );
+}
+
+/* function Msg({ name, img, id }) {
   const [count, setCount] = useState(0);
   return (
     <div className="back">
@@ -42,6 +146,6 @@ function Msg({ name, img, id }) {
       </div>
     </div>
   );
-}
+} */
 
 export default App;
